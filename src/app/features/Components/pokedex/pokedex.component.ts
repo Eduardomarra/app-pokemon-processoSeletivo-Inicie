2ed0277600bf0fe615314aa00c1d34c4fc2e0e78
Eduardo Marra <eduardomarra@gmail.com>
2023-05-25 17:12:08 -0300

    created services pokedex

--- src/app/features/Components/pokedex/pokedex.component.ts
+++ src/app/features/Components/pokedex/pokedex.component.ts
@@ -1,83 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiPokemonService } from '../../services/api-pokemon.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-pokedex',
   templateUrl: './pokedex.component.html',
   styleUrls: ['./pokedex.component.css']
 })
-export class PokedexComponent {
+export class PokedexComponent implements OnInit{
 
-  lista = [
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
-    },
-    {
-      name: "Charmeleon",
-      cod: "0001",
-      src: "../../../../assets/img-header/charmeleon.svg",
-      heightPoke: 1.10,
-      weightPoke: 19
+  endPoint: number = 0;
+  limited: number = 10;
+  public getAllPokemons: any;
+
+  constructor(private apiPokemonService: ApiPokemonService){}
+
+  ngOnInit():void {
+    this.apiGetAllPokemons();
+  }
+
+  public apiGetAllPokemons():void {
+     this.apiPokemonService.getAllpokemons(this.endPoint, this.limited).subscribe(
+      (res) => {
+        this.getAllPokemons = res.results
+        console.log( this.getAllPokemons)
+      }
+    );
+  }
+
+  public nextPagePokemon() {
+    if(this.endPoint == 90 ) {
+      return
+
     }
-  ];
+    this.endPoint += 10;
+    this.apiGetAllPokemons();
+  }
 
+  public prevPagePokemon() {
+    if(this.endPoint ==0 ) {
+      return
+    }
+    this.endPoint -= 10;
+    this.apiGetAllPokemons();
+  }
 }
---
+++ src/app/features/services/api-pokemon.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap, map } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiPokemonService {
+
+  private baseURL: string = 'https://pokeapi.co/api/v2/pokemon/';
+
+  constructor(private httpClient: HttpClient) { }
+
+  public getAllpokemons(endPoint: number, limited: number): Observable<any> {
+    return this.httpClient.get<any>(`${this.baseURL}?offset=${endPoint}&limit=${limited}`).pipe(
+      tap(res => res),
+      tap(res => {
+        res.results.map((resPokemons:any) => {
+          this.apiGetPokemons(resPokemons.url).subscribe(
+            res => resPokemons.status = res
+          )
+        })
+      })
+    )
+  }
+
+  public apiGetPokemons(url: string):Observable<any>{
+    return this.httpClient.get<any>(url).pipe(
+      map(
+        res => res
+      )
+    )
+  }
+}
